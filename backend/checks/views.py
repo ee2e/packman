fa1bce269e42edeac7e0d68b4b1ee0c498b4686e
강창현 <aaakch0316@gmail.com>
2020-09-16 14:25:31 +0900

    S03P22D208-161 [+]  "Check_list.user" must be a "User" instance

--- backend/checks/views.py
+++ backend/checks/views.py
@@ -21,5 +21,5 @@ def check_detail(request, check_pk):
 def create_check(request):
     serializer = CheckSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
-        serializer.save(user_id=1)
+        serializer.save(user=request.user)  # NOT NULL CONSTRAINT FAILD
         return Response(serializer.data)
