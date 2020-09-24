a6205fd7808c0182516b5419a3f6f6f3cc5d9b0e
ee2e <ee2e@naver.com>
2020-09-24 16:30:10 +0900

    S03P23D208-15 [+] edit login

--- frontend/PackMan/components/Gate.js
+++ frontend/PackMan/components/Gate.js
@@ -2,11 +2,13 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Auth from "./Auth/Auth";
 import Main from "./Main/Main";
+import { useSelector } from "react-redux";
 
 function Gate() {
+  const { isLoggedIn } = useSelector((state) => state.usersReducer);
   return (
     <NavigationContainer>
-      <Auth />
+      {isLoggedIn ? <Main /> : <Auth />}
     </NavigationContainer>
   );
 }
