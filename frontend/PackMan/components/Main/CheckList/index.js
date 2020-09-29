964b12a22d09084fd34cf86500ea27c6380e2a82
yeoro <dufgh1009@naver.com>
2020-09-29 14:23:33 +0900

    S03P23D208-85 [+] edit Align

--- frontend/PackMan/components/Main/CheckList/index.js
+++ frontend/PackMan/components/Main/CheckList/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { EvilIcons, MaterialIcons } from "@expo/vector-icons";
-import { Button, Input } from "react-native-elements";
+import { MaterialIcons } from "@expo/vector-icons";
 import moment from 'moment';
 import { 
   View, 
@@ -8,7 +7,8 @@ import {
   StyleSheet,
   TouchableWithoutFeedback,
   KeyboardAvoidingView,
-  Keyboard
+  Keyboard,
+  TextInput
  } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import DateTimePicker from "react-native-modal-datetime-picker";
@@ -25,6 +25,15 @@ export default class CheckList extends Component {
     }
   }
 
+  // 컴포넌트 이동 시 일정, 날짜 초기화
+  reset = () => {
+    this.setState({
+      content: '',
+      chosenDate: '날짜를 선택해주세요!'
+    })
+  }
+
+  // 날짜 선택
   handleDatePicker = (datetime) => {
     this.setState({
       isVisible: false,
@@ -32,18 +41,21 @@ export default class CheckList extends Component {
     })
   }
 
+  // 날짜 선택창 보여주기
   showDatePicker = () => {
     this.setState({
       isVisible: true
     })
   }
 
+  // 날짜 선택창 숨기기
   hideDatePicker = () => {
     this.setState({
       isVisible: false
     })
   }
 
+  // 일정 입력 여부 확인
   isFormValid = () => {
     const {
       content
@@ -57,6 +69,7 @@ export default class CheckList extends Component {
     return true;
   };
 
+  // 체크리스트 작성
   writeCheckList = async (event) => {
     const { content } = this.state;
 
@@ -65,9 +78,8 @@ export default class CheckList extends Component {
     }
 
     try {
-      if (status === 201) {
-        alert("체크리스트 작성 완료");
-      }
+      this.resetDate();
+      alert("체크리스트 작성 완료");
     } catch (event) {
       alert("체크리스트 작성 실패");
     }
@@ -79,47 +91,48 @@ export default class CheckList extends Component {
       content
     } = this.state;
     return (
-
-      <View style={styles.container}>
-        <View style={styles.top}>
-          <MaterialIcons
-            name="close"
-            size={30}
-            color="black"
-            style={{ marginTop: 30, marginBottom: 10, marginLeft: 13 }}
-            onPress={() => navigation.goBack()}
-          />
-          <Text style={styles.top_text}>일정</Text>
-          <MaterialIcons
-            name="check"
-            size={30}
-            color="black"
-            style={{ marginTop: 30, marginBottom: 10, marginRight: 13 }}
-            onPress={() => navigation.goBack()}
-          />
-        </View>
-        <KeyboardAvoidingView
-          behavior={Platform.OS == "ios" ? "padding" : "height"}
-          style={styles.container}
-        >
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-              <View>
-                <Input
-                  value={content}
-                  onChangeText={(content) => this.setState({ content })}
-                  containerStyle={styles.inputContainer}
-                  inputStyle={styles.input}
-                  placeholder="일정을 입력하세요."
-                  // onSubmitEditing={() => this.passwordInput.focus()}
-                />
-              </View>
-          </TouchableWithoutFeedback>
-
-          <View>
-            <TouchableOpacity onPress={this.showDatePicker}>
-              <Button 
-                title={this.state.chosenDate}
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+        style={styles.container}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.container}>
+            <View style={styles.top}>
+              <MaterialIcons
+                name="close"
+                size={30}
+                color="black"
+                style={{ marginTop: 30, marginBottom: 10, marginLeft: 13 }}
+                onPress={() => {
+                  this.reset()
+                  navigation.goBack()
+                }}
               />
+              <Text style={styles.top_text}>일정</Text>
+              <MaterialIcons
+                name="check"
+                size={30}
+                color="black"
+                style={{ marginTop: 30, marginBottom: 10, marginRight: 13 }}
+                onPress={this.writeCheckList}
+              />
+            </View>
+
+            <TextInput
+              style={styles.input}
+              value={content}
+              onChangeText={(content) => this.setState({ content })}
+              placeholder="일정을 입력하세요."
+              multiline={true}
+              onEndmitEditing={Keyboard.dismiss}
+            />
+            <TouchableOpacity 
+              style={styles.date_button}
+              onPress={this.showDatePicker}
+            >
+              <Text
+                style={styles.date_text}
+              > {this.state.chosenDate} </Text>
             </TouchableOpacity>
             <DateTimePicker
               isVisible={this.state.isVisible}
@@ -127,8 +140,8 @@ export default class CheckList extends Component {
               onCancel={this.hideDatePicker}
             />
           </View>
-        </KeyboardAvoidingView>
-    </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -143,8 +156,31 @@ const styles = StyleSheet.create({
   },
   top_text: {
     fontFamily: "BMHANNA",
-    marginTop: 21,
+    marginTop: 35,
     fontSize: 20
   },
+  input: {
+    fontFamily: "BMHANNA",
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20,
+    height: 70,
+    fontSize: 20
+  },
+  date_button: {
+    height: 50,
+    backgroundColor: '#ffe14b',
+    fontSize: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 50,
+    marginRight: 50
+  },
+  date_text: {
+    fontFamily: "BMHANNA",
+    fontSize: 20,
+  }
   
 });
