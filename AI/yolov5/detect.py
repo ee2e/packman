8b6b833058c0524b75aca1100c599a2fd1016738
강창현 <aaakch0316@gmail.com>
2020-10-05 14:08:42 +0900

    S03P23D208-108 [+] change settings

--- AI/yolov5/detect.py
+++ AI/yolov5/detect.py
@@ -142,7 +142,7 @@ def detect(save_img=False):
                 pprint(json_data)
                 print(type(json_data))
 
-                with open('./inference/output/labels.json', 'w', encoding='utf-8') as make_file:
+                with open(save_path + '.json', 'w', encoding='utf-8') as make_file:
                     json.dump(data, make_file, ensure_ascii=False, indent='\t')
 
             # Print time (inference + NMS)
@@ -182,13 +182,12 @@ def detect(save_img=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-<<<<<<< HEAD
     parser.add_argument('--weights', nargs='+', type=str,
-                        default='/weights/best.pt', help='model.pt path(s)')
+                        default='../AI/yolov5/weights/best.pt', help='model.pt path(s)')
     # file/folder, 0 for webcam
     parser.add_argument('--source', type=str,
-                        default='inference/images', help='source')
-    parser.add_argument('--output', type=str, default='inference/output',
+                        default='../AI/yolov5/inference/images', help='source')
+    parser.add_argument('--output', type=str, default='../AI/yolov5/inference/output',
                         help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=416,
                         help='inference size (pixels)')
@@ -211,22 +210,6 @@ if __name__ == '__main__':
     parser.add_argument('--update', action='store_true',
                         help='update all models')
 
-=======
-    parser.add_argument('--weights', nargs='+', type=str, default='weights/best.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
-    parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.2, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    
->>>>>>> 963387b93a8f10c52f3b3bc2bb66839ec03dedf7
     opt = parser.parse_args()
     print(opt)
 
