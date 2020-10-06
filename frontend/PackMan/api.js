bb522bb2aa2e9008b6017aa71bf63c2e0e819a9a
yeoro <dufgh1009@naver.com>
2020-10-06 22:46:32 +0900

    test

--- frontend/PackMan/api.js
+++ frontend/PackMan/api.js
@@ -5,7 +5,7 @@ const callApi = async (method, path, data, jwt, params = {}) => {
     Authorization: `Bearer ${jwt}`,
     "Content-Type": "application/json",
   };
-  const baseUrl = "http://127.0.0.1:8000/api/v1";
+  const baseUrl = "http://172.20.10.2:8000/api/v1";
   const fullUrl = `${baseUrl}${path}`;
   if (method === "get" || method === "delete") {
     return axios[method](fullUrl, { headers, params });
