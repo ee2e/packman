1f5469aa8ad6755145140c9e1bbd121c1adf5328
yeoro <dufgh1009@naver.com>
2020-10-06 21:10:53 +0900

    push

--- backend/utilities/views.py
+++ backend/utilities/views.py
@@ -12,7 +12,9 @@ import os
 
 #s3
 import boto3
-
+import json
+from botocore.exceptions import ClientError
+import logging
 
 # json return
 # from django.http import HttpResponse
@@ -24,10 +26,10 @@ def index(request):
 
 
 
-AWS_ACCESS_KEY_ID = "AKIA3IDIIPPFDKAGJHHJ"
-AWS_SECRET_ACCESS_KEY = "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"
+AWS_ACCESS_KEY_ID = "AKIAIVPM3UIRJJ47XPNQ"
+AWS_SECRET_ACCESS_KEY = "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"
 AWS_DEFAULT_REGION = "ap-northeast-2"
-AWS_BUCKET_NAME = "packmanpy"
+AWS_BUCKET_NAME = "pack-man"
 # import xmltodict
 # import json
 
@@ -36,10 +38,14 @@ AWS_BUCKET_NAME = "packmanpy"
 @api_view(['POST'])
 def detect(request):
     url = request.data.get('url')
-    print(url)
-    start = str(int(time.time()))
-    print(start)
-    storage = "> ../AI/yolov5/inference/images/" + start + ".jpg"
+    print('url :', url)
+
+    ## count
+    cnt = len('https://pack-man.s3.ap-northeast-2.amazonaws.com/')
+    filename = url[cnt:]
+    print('file :', filename)
+
+    storage = "> ../AI/yolov5/inference/images/" + filename
     print("Storage : " + storage)
     os.system("curl " + url + storage)
     os.system("python ../AI/yolov5/detect.py")
@@ -51,21 +57,74 @@ def detect(request):
 
 
     # ap-northeast-2
-    s3 = boto3.client('s3')
-    filename = start + ".jpg"
-    fileurl = "../backend/static/"+filename
-    bucket_name = 'packmanpy'
-    s3.upload_file(fileurl, bucket_name, filename)
+    fileurl = "../backend/static/" + filename
+    s3 = boto3.client('s3',
+        aws_access_key_id=AWS_ACCESS_KEY_ID, 
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=AWS_DEFAULT_REGION
+    )
+    try:
+        response = s3.upload_file(fileurl, AWS_BUCKET_NAME, filename)
+    except ClientError as e:
+        logging.error(e)
+        return false
+    return true
+    
+    # s3.upload_file(fileurl, bucket_name, filename)
     # os.system("curl " + url + " > ../AI/yolov5/inference/images/a.jpg")
-
-
-
 
     dicurlname = fileurl + '.json'
     dicurl = open(dicurlname)
     data = json.load(dicurl)
-    pprint(data)
+    print(type(data))
+    print(data)
+    print(data['labels'])
+    print(data['labels'][0])
 
+    final_list = []
+    input_list = [
+        "socks",
+        "ballcap",
+        "hoody",
+        "pants",
+        "hair dryer",
+        "mask",
+        "charger",
+        "backpack",
+        "T-shirt"
+    ]
+    outlist_list = [
+        '양말', 
+        '모자', 
+        '후드', 
+        '바지', 
+        '드라이기', 
+        '마스크', 
+        '충전기', 
+        '가방', 
+        '티셔츠'
+    ]
 
+    if data['labels']:
+        for label in data['labels']:
+            for i in range(len(input_list)):
+                if label == input_list[i]:
+                    final_list.append(outlist_list[i])
+    print(final_list)
 
-    return HttpResponse('ok')
+#     "socks", 양말
+# "ballcap", 모자
+# "hoody", 후드
+# "pants", 바지
+# "hair dryer", 드라이기
+# "mask", 마스크
+# "charger", 충전기
+# "backpack", 가방
+# "T-shirt", 티셔츠
+
+    return Response(data={"stuff_list": final_list})
+
+    # encoded_jwt = jwt.encode(
+    #     {"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256"
+    # )
+    # return Response(data={"token": encoded_jwt, "id": user.pk})
